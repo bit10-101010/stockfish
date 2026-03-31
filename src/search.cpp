ffa7cfca038c61a41d08bc613b67a5068b1524ee
Bit <bit10101010@outlook.com>
2026-04-01 07:17:13 +1100

    search: reduce parent futility pruning aggressiveness

--- src/search.cpp
+++ src/search.cpp
@@ -1114,7 +1114,7 @@ moves_loop:  // When in check, search starts here
                 // (*Scaler): Generally, lower divisors scales well
                 lmrDepth += history / 2995;
 
-                Value futilityValue = ss->staticEval + 42 + 151 * !bestMove + 120 * lmrDepth
+                Value futilityValue = ss->staticEval + 48 + 151 * !bestMove + 120 * lmrDepth
                                     + 86 * (ss->staticEval > alpha);
 
                 // Futility pruning: parent node
